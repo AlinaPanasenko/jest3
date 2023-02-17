8b45e8b800b62d19d0398c442de2daea65af3d26
AlinaPanasenko <maikshev@gmail.com>
2023-02-17 22:43:16 +0000

    Add another test

--- scripts/tests/button.test.js
+++ scripts/tests/button.test.js
@@ -7,7 +7,11 @@ const buttonClick = require("../button");
 
 
 beforeAll(() => {
-    document.body.innerHTML = "<p id='par'></p>";
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
 });
 
 describe("DOM tests", () => {
@@ -15,5 +19,9 @@ describe("DOM tests", () => {
         buttonClick();
         expect(document.getElementById("par")
         .innerHTML).toEqual("You clicked");
+    });
+
+    test("h1 should exist", () => {
+        expect(document.getElementsByTagName("h1").length).toBe(1);
     });
 });
